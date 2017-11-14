16713931f0cb012ae91e0971a22c82ed3464a250
inhyun kim <illyce1@naver.com>
2017-11-14 01:35:50 -0800

    second add header

--- header_ih/tetris.h
+++ header_ih/tetris.h
@@ -1,7 +1,23 @@
 #include<stdio.h>
 #include<string.h>
+#include<signal.h>
+#include<time.h>
+
+#define LEFT 0
+#define RIGHT 1
+#define DOWN 2
+#define UP 3
+
+#define I_BLOCK 0
+#define T_BLOCK 1
+#define Z_BLOCK 2
+#define S_BLOCK 3
+#define L1_BLOCK 4
+#define L2_BLOCK 5
+#define O_BLOCK 6
 
 void block_shape(void);
+int collision(int command);
 
 extern char I_block[4][4][4];
 extern char T_block[4][4][4];
@@ -17,5 +33,3 @@ extern int y;
 extern int block_state;
 extern int block_num;
 extern int next_block_num;
-extern int point;
-extern int game;
