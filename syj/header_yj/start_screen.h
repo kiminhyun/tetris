194c1c671585e7961b67842bd638128f3a72c275
yoojin0110 <yoojin0110@naver.com>
2017-11-23 08:29:18 -0800

    correct

--- syj/header_yj/start_screen.h
+++ syj/header_yj/start_screen.h
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int start_screen(void);
+
 int start_screen(void)
 {
-
-	int menu;
+	int speed;
 
         system("clear");
         printf("************Tetris*************\n");
-        printf("\n\n\n\n\n");
-        printf("***********************\n");
-        printf("1. game_start\n");
-        printf("2. game_end\n");
-        printf("***********************\n");
-        printf("select menu : ");
-	scanf("%d",&menu);
-	return menu;
+	printf("Speed Level (1~10) : ");
+	scanf("%d",&speed);
+	return speed;
 }
